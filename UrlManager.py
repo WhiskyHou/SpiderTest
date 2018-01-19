6715ab5b8e75b37b593b67bd28a07b81a0958345
houyi <ghostgaz@126.com>
2018-01-19 15:04:32 +0800

    2018/1/19

--- UrlManager.py
+++ UrlManager.py
@@ -1,22 +1,29 @@
 # encoding=utf-8
 
+import cPickle
+import hashlib
 
 class UrlManager(object):
     def __init__(self):
-        self.new_urls = set()
-        self.old_urls = set()
+        self.new_urls = self.load_progress('new_urls.txt')
+        self.old_urls = self.load_progress('old_urls.txt')
 
     def has_new_url(self):
         return self.new_url_size() != 0
 
     def get_new_url(self):
         new_url = self.new_urls.pop()
-        self.old_urls.add(new_url)
+        m = hashlib.md5()
+        m.update(new_url)
+        self.old_urls.add(m.hexdigest()[8:-8])
         return new_url
 
     def add_new_url(self, url):
         if url is None:
             return
+        m = hashlib.md5()
+        m.update(url)
+        url_md5 = m.hexdigest()[8:-8]
         if url not in self.new_urls and url not in self.old_urls:
             self.new_urls.add(url)
 
@@ -31,3 +38,16 @@ class UrlManager(object):
 
     def old_url_size(self):
         return len(self.old_urls)
+
+    def save_progress(self, path, data):
+        with open(path, 'wb') as f:
+            cPickle.dump(data, f)
+
+    def load_progress(self, path):
+        try:
+           with open(path, 'rb') as f:
+               temp = cPickle.load(f)
+               return temp
+        except:
+            print("file load failed "+path)
+        return set()
