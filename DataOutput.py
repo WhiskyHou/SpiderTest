c14cc0457e1edfadb95d9ed5a2c69c5df2c13c45
houyi <ghostgaz@126.com>
2018-01-19 15:03:39 +0800

    2018/1/19

--- DataOutput.py
+++ DataOutput.py
@@ -1,30 +1,43 @@
 # encoding=utf-8
 
 import codecs
+import time
 
 
 class DataOutput(object):
     def __init__(self):
+        self.filepath = 'baidu2_%s.html'%(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
+        self.output_head(self.filepath)
         self.datas = []
+
+    def output_head(self, path):
+        fout = codecs.open(path, 'w', encoding='utf-8')
+        fout.write("<html>")
+        fout.write("<head><meta charset='utf-8'/></head>")
+        fout.write("<body>")
+        fout.write("<table>")
+        fout.close()
 
     def store_data(self, data):
         if data is None:
             return
         self.datas.append(data)
+        if len(self.datas) > 10:
+            self.output_html(self.filepath)
 
-    def output_html(self):
-        fout = codecs.open('baidu.html', 'w', encoding='utf-8')
-        fout.write("<html>")
-        fout.write("<head><meta charset='utf-8'/></head>")
-        fout.write("<body>")
-        fout.write("<table>")
+    def output_html(self, path):
+        fout = codecs.open(path, 'a', encoding='utf-8')
         for data in self.datas:
             fout.write("<tr>")
             fout.write("<td>%s</td>"%data['url'])
             fout.write("<td>%s</td>"%data['title'])
             fout.write("<td>%s</td>"%data['summary'])
             fout.write("</tr>")
-            # self.datas.remove(data)
+            self.datas.remove(data)
+        fout.close()
+
+    def output_end(self, path):
+        fout = codecs.open(path, 'a', encoding='utf-8')
         fout.write("</table>")
         fout.write("</body>")
         fout.write("</html>")
