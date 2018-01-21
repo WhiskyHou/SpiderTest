322375fd054c6f16e3fdffb111c8e8aa73533b0c
houyi <ghostgaz@126.com>
2018-01-21 16:16:01 +0800

    2018/1/21

--- mysql.py
+++ mysql.py
@@ -12,6 +12,15 @@ char = "utf8"
 
 connection = MySQLdb.connect(host=hostname, user=user, passwd=password, db=dbname, port=port, charset=char)
 cursor = connection.cursor()
+# cursor.execute('SELECT * FROM data')
+# data = cursor.fetchone()
+# print data
+try:
+    cursor.execute('INSERT INTO data(url, title, content) VALUES("http://baidu.com", "baidu", "a famous it company")')
+    connection.commit()
+    print("insert success")
+except:
+    print("insert error")
 cursor.execute('SELECT * FROM data')
-data = cursor.fetchone()
-print data
+data2 = cursor.fetchall()
+print(data2)
